e0c3cd5157f74236c6ecd74d922b1ff9d6c8b59b
jmeisele <jmeisele@yahoo.com>
2024-11-25 09:26:39 -0600

    :fire:

--- data.tf
+++ data.tf
@@ -1,84 +1,84 @@
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
+# data "aws_region" "current" {}
+# data "aws_caller_identity" "current" {}
 
-data "aws_iam_policy_document" "datasync_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
+# data "aws_iam_policy_document" "datasync_assume_role_policy" {
+#   statement {
+#     actions = ["sts:AssumeRole"]
 
-    principals {
-      type        = "Service"
-      identifiers = ["datasync.amazonaws.com"]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "aws:SourceAccount"
-      values   = [var.account_id]
-    }
-  }
-}
+#     principals {
+#       type        = "Service"
+#       identifiers = ["datasync.amazonaws.com"]
+#     }
+#     condition {
+#       test     = "StringEquals"
+#       variable = "aws:SourceAccount"
+#       values   = [var.account_id]
+#     }
+#   }
+# }
 
-data "aws_iam_policy_document" "allow_access_from_datasync_role" {
-  statement {
-    actions = [
-      "s3:GetBucketLocation",
-      "s3:ListBucket",
-      "s3:ListBucketMultipartUploads"
-    ]
-    principals {
-      type        = "AWS"
-      identifiers = [aws_iam_role.datasync.arn]
-    }
-    resources = [aws_s3_bucket.this.arn]
-  }
-  statement {
-    sid    = "BucketObjects"
-    effect = "Allow"
-    principals {
-      type        = "AWS"
-      identifiers = [aws_iam_role.datasync.arn]
-    }
-    actions = [
-      "s3:AbortMultipartUpload",
-      "s3:DeleteObject",
-      "s3:GetObject",
-      "s3:GetObjectTagging",
-      "s3:GetObjectVersion",
-      "s3:GetObjectVersionTagging",
-      "s3:ListMultipartUploadParts",
-      "s3:PutObject",
-      "s3:PutObjectTagging"
-    ]
-    resources = [
-      "${aws_s3_bucket.this.arn}/*"
-    ]
-  }
-}
+# data "aws_iam_policy_document" "allow_access_from_datasync_role" {
+#   statement {
+#     actions = [
+#       "s3:GetBucketLocation",
+#       "s3:ListBucket",
+#       "s3:ListBucketMultipartUploads"
+#     ]
+#     principals {
+#       type        = "AWS"
+#       identifiers = [aws_iam_role.datasync.arn]
+#     }
+#     resources = [aws_s3_bucket.this.arn]
+#   }
+#   statement {
+#     sid    = "BucketObjects"
+#     effect = "Allow"
+#     principals {
+#       type        = "AWS"
+#       identifiers = [aws_iam_role.datasync.arn]
+#     }
+#     actions = [
+#       "s3:AbortMultipartUpload",
+#       "s3:DeleteObject",
+#       "s3:GetObject",
+#       "s3:GetObjectTagging",
+#       "s3:GetObjectVersion",
+#       "s3:GetObjectVersionTagging",
+#       "s3:ListMultipartUploadParts",
+#       "s3:PutObject",
+#       "s3:PutObjectTagging"
+#     ]
+#     resources = [
+#       "${aws_s3_bucket.this.arn}/*"
+#     ]
+#   }
+# }
 
-data "aws_iam_policy_document" "s3_policy" {
-  statement {
-    actions = [
-      "s3:GetBucketLocation",
-      "s3:ListBucket",
-      "s3:ListBucketMultipartUploads"
-    ]
-    resources = [aws_s3_bucket.this.arn]
-  }
-  statement {
-    sid    = "BucketObjects"
-    effect = "Allow"
-    actions = [
-      "s3:AbortMultipartUpload",
-      "s3:DeleteObject",
-      "s3:GetObject",
-      "s3:GetObjectTagging",
-      "s3:GetObjectVersion",
-      "s3:GetObjectVersionTagging",
-      "s3:ListMultipartUploadParts",
-      "s3:PutObject",
-      "s3:PutObjectTagging"
-    ]
-    resources = [
-      "${aws_s3_bucket.this.arn}/*"
-    ]
-  }
-}
+# data "aws_iam_policy_document" "s3_policy" {
+#   statement {
+#     actions = [
+#       "s3:GetBucketLocation",
+#       "s3:ListBucket",
+#       "s3:ListBucketMultipartUploads"
+#     ]
+#     resources = [aws_s3_bucket.this.arn]
+#   }
+#   statement {
+#     sid    = "BucketObjects"
+#     effect = "Allow"
+#     actions = [
+#       "s3:AbortMultipartUpload",
+#       "s3:DeleteObject",
+#       "s3:GetObject",
+#       "s3:GetObjectTagging",
+#       "s3:GetObjectVersion",
+#       "s3:GetObjectVersionTagging",
+#       "s3:ListMultipartUploadParts",
+#       "s3:PutObject",
+#       "s3:PutObjectTagging"
+#     ]
+#     resources = [
+#       "${aws_s3_bucket.this.arn}/*"
+#     ]
+#   }
+# }
--- main.tf
+++ main.tf
@@ -1,79 +1,79 @@
-resource "aws_s3_bucket" "this" {
-  bucket_prefix = "aws-datasync-on-prem-bucket"
-  force_destroy = true
-}
+# resource "aws_s3_bucket" "this" {
+#   bucket_prefix = "aws-datasync-on-prem-bucket"
+#   force_destroy = true
+# }
 
-resource "aws_s3_bucket_ownership_controls" "this" {
-  bucket = aws_s3_bucket.this.id
-  rule {
-    object_ownership = "BucketOwnerEnforced"
-  }
-}
+# resource "aws_s3_bucket_ownership_controls" "this" {
+#   bucket = aws_s3_bucket.this.id
+#   rule {
+#     object_ownership = "BucketOwnerEnforced"
+#   }
+# }
 
-resource "aws_s3_bucket_policy" "allow_access_from_datasync_role" {
-  bucket = aws_s3_bucket.this.id
-  policy = data.aws_iam_policy_document.allow_access_from_datasync_role.json
-}
+# resource "aws_s3_bucket_policy" "allow_access_from_datasync_role" {
+#   bucket = aws_s3_bucket.this.id
+#   policy = data.aws_iam_policy_document.allow_access_from_datasync_role.json
+# }
 
-resource "aws_iam_role" "datasync" {
-  name               = "datasync_role"
-  path               = "/"
-  assume_role_policy = data.aws_iam_policy_document.datasync_assume_role_policy.json
-}
+# resource "aws_iam_role" "datasync" {
+#   name               = "datasync_role"
+#   path               = "/"
+#   assume_role_policy = data.aws_iam_policy_document.datasync_assume_role_policy.json
+# }
 
-resource "aws_iam_policy" "s3_policy" {
-  name        = "s3_policy"
-  path        = "/"
-  description = "Allows role to interact with bucket"
-  policy      = data.aws_iam_policy_document.s3_policy.json
-}
+# resource "aws_iam_policy" "s3_policy" {
+#   name        = "s3_policy"
+#   path        = "/"
+#   description = "Allows role to interact with bucket"
+#   policy      = data.aws_iam_policy_document.s3_policy.json
+# }
 
-resource "aws_iam_policy_attachment" "s3_attachment" {
-  name       = "s3-policy-attachment"
-  roles      = [aws_iam_role.datasync.name]
-  policy_arn = aws_iam_policy.s3_policy.arn
-}
+# resource "aws_iam_policy_attachment" "s3_attachment" {
+#   name       = "s3-policy-attachment"
+#   roles      = [aws_iam_role.datasync.name]
+#   policy_arn = aws_iam_policy.s3_policy.arn
+# }
 
-resource "aws_datasync_location_s3" "staging" {
-  s3_bucket_arn = aws_s3_bucket.this.arn
-  subdirectory  = "sl1"
-  s3_config {
-    bucket_access_role_arn = aws_iam_role.datasync.arn
-  }
-}
+# resource "aws_datasync_location_s3" "staging" {
+#   s3_bucket_arn = aws_s3_bucket.this.arn
+#   subdirectory  = "sl1"
+#   s3_config {
+#     bucket_access_role_arn = aws_iam_role.datasync.arn
+#   }
+# }
 
-resource "aws_datasync_agent" "this" {
-  activation_key = "HB8C4-QC0CU-V23HT-E16O0-R9B1D"
-  name           = "jason-mbp"
-}
+# resource "aws_datasync_agent" "this" {
+#   activation_key = "HB8C4-QC0CU-V23HT-E16O0-R9B1D"
+#   name           = "jason-mbp"
+# }
 
-resource "aws_datasync_location_smb" "local_smb" {
-  server_hostname = "192.168.1.66"
-  subdirectory    = "/50-days-rust"
-  user            = "jasoneisele"
-  password        = var.password
-  agent_arns      = [aws_datasync_agent.this.arn]
-}
+# resource "aws_datasync_location_smb" "local_smb" {
+#   server_hostname = "192.168.1.66"
+#   subdirectory    = "/50-days-rust"
+#   user            = "jasoneisele"
+#   password        = var.password
+#   agent_arns      = [aws_datasync_agent.this.arn]
+# }
 
-resource "aws_datasync_task" "xfer_rust" {
-  destination_location_arn = aws_datasync_location_s3.staging.arn
-  name                     = "xfer_rust"
-  source_location_arn      = aws_datasync_location_smb.local_smb.arn
-  # schedule {
-  #   schedule_expression = "cron(0 12 ? * SUN,WED *)"
-  # }
-  options {
-    bytes_per_second  = -1
-    posix_permissions = "NONE"
-    uid               = "NONE"
-    gid               = "NONE"
-  }
-  # excludes {
-  #   filter_type = "SIMPLE_PATTERN"
-  #   value       = "/folder1|/folder2"
-  # }
-  # includes {
-  #   filter_type = "SIMPLE_PATTERN"
-  #   value       = "/folder1|/folder2"
-  # }
-}
+# resource "aws_datasync_task" "xfer_rust" {
+#   destination_location_arn = aws_datasync_location_s3.staging.arn
+#   name                     = "xfer_rust"
+#   source_location_arn      = aws_datasync_location_smb.local_smb.arn
+#   # schedule {
+#   #   schedule_expression = "cron(0 12 ? * SUN,WED *)"
+#   # }
+#   options {
+#     bytes_per_second  = -1
+#     posix_permissions = "NONE"
+#     uid               = "NONE"
+#     gid               = "NONE"
+#   }
+#   # excludes {
+#   #   filter_type = "SIMPLE_PATTERN"
+#   #   value       = "/folder1|/folder2"
+#   # }
+#   # includes {
+#   #   filter_type = "SIMPLE_PATTERN"
+#   #   value       = "/folder1|/folder2"
+#   # }
+# }
